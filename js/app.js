674ebdc00a6cf4800737122cfa0e331b09abacc5
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-06-21 13:37:39 +0200

    add h1 + img

--- js/app.js
+++ js/app.js
@@ -2,3 +2,12 @@
  * Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
  * Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
  */
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello World!',
+        img: 'https://www.trovafrasi.com/wp-content/uploads/2019/12/frasi-sull-estate-700x490.jpg',
+        bkground: 'background-green'
+    }
+});
